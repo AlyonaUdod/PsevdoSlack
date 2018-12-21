ec335929eb501ddb23a3a73224a7c73a9c25fa54
Your Name <freelspringisoul@gmail.com>
2018-12-21 13:13:03 +0200

    markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import ColorPanel from './ColorPanel/ColorPanel'
+import SidePanel from './SidePanel/SidePanel'
+import Message from './Message/Message'
+import MetaPanel from './MetaPanel/MetaPanel'
+import { Grid } from 'semantic-ui-react';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-          App
-      </div>
+      <Grid columns='equal' className="app">
+          <ColorPanel/>
+          <SidePanel/>
+        <Grid.Column textAlign='center'>
+          <Message/>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <MetaPanel/>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
