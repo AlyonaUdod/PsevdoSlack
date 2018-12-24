4957a75ff9e8dbde0b5af74b8a66fa0bb6d439fb
Your Name <freelspringisoul@gmail.com>
2018-12-24 14:47:42 +0200

    color_panel_markup

--- src/ColorPanel/ColorPanel.jsx
+++ src/ColorPanel/ColorPanel.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import { Sidebar, Divider, Button, Menu } from 'semantic-ui-react';
 
 export default class ColorPanel extends Component {
   render() {
     return (
-      <div>
-        ColorPanel
-      </div>
+      <Sidebar
+        as={Menu}
+        icon='labeled'
+        inverted
+        visible
+        vertical
+        width='very thin'>
+        <Divider/>
+          <Button icon='add' size='small' color='blue'/>
+      </Sidebar>
     )
   }
 }
