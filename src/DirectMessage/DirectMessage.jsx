6fd813b2726d2942cc5af6b660f8e9b556f88c69
Your Name <freelspringisoul@gmail.com>
2018-12-25 16:16:42 +0200

    direct

--- src/DirectMessage/DirectMessage.jsx
+++ src/DirectMessage/DirectMessage.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 import {Menu, Icon} from 'semantic-ui-react'
+import firebase from '../firebase'
+import {connect} from 'react-redux'
 
 class DirectMessage extends Component {
 
     state = {
-        users: []
+        users: [],
+        usersRef: firebase.database().ref('users')
+    }
+
+    componentDidMount() {
+        if (this.props.user) {
+            this.addListener(this.props.user.uid)
+        }
+    }
+
+    addListener = id => {
+        let loadedUsers = [];
+        this.state.usersRef.on('child_added',snap => {   // snap = объект огромный, который нам прилетает после on.
+            if(id !== snap.key) {
+                let user = snap.val(); // запускаем фукнцию val которая возвращет объект юзера
+                user.uid = snap.key;
+                user.status = 'offline'
+                loadedUsers.push(user)
+                this.setState({
+                    users: loadedUsers,
+                })
+            }
+        })
     }
 
   render() {
@@ -16,9 +40,23 @@ class DirectMessage extends Component {
                 <Icon name='mail'/> DIRECT MESSAGES 
             </span> ({users.length})
         </Menu.Item>
+        {users.map(el => <Menu.Item
+        key={el.uid}
+        onClick={() => console.log('lll')}
+        style={{opacity:.7, fontStyle:'italic'}}
+        >
+        <Icon name='circle'/>
+        @ {el.name}
+        </Menu.Item>)}
      </Menu.Menu>
     )
   }
 }
 
-export default DirectMessage
+function MSTP (state) {
+    return {
+        user: state.user.currentUser,
+    }
+}
+
+export default connect(MSTP)(DirectMessage)
